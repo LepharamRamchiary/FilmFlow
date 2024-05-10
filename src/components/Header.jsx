6240a0f51bbbfb05b2266fc493802c7631adcf89
Lepha <lepharamchiary@gmail.com>
2024-05-10 11:37:09 +0530

    header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "./NavBar";
+
+const Header = () => {
+  return (
+    <div className="flex flex-col">
+      <div className="bg-slate-800 w-full md:h-24 lg:h-20 py-4 px-10 md:py-3 md:px-8 lg:py-4 lg:px-10">
+        <h1 className="text-white font-bold text-2xl md:text-4xl lg:text-5xl">
+          FlimFlow
+        </h1>
+      </div>
+      <NavBar />
+    </div>
+  );
+};
+
+export default Header;
