848ae880f0157bbd709bbfa6632aec3e4a02aae0
Lepha <lepharamchiary@gmail.com>
2024-05-13 22:24:53 +0530

    simple addition

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
 import Feed from "./components/Feed";
+import Details from './components/Details';
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Feed />
+      <Routes>
+        <Route path='/' exact element={<Feed />} />
+        <Route path='/video' element={<Details />} />
+      </Routes>
     </BrowserRouter>
   );
 }
