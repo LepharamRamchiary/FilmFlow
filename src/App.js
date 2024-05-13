bd2bc742931c0f8df8aeb539f4468713fade97da
Lepha <lepharamchiary@gmail.com>
2024-05-13 13:38:15 +0530

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './index.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import Feed from "./components/Feed";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Feed />
     </BrowserRouter>
   );
 }
