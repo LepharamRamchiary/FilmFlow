08a6d5a42b7cd2dcd5db32460e38c0a3d8a86c77
Lepha <lepharamchiary@gmail.com>
2024-05-15 13:23:49 +0530

    .

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,120 +1,26 @@
-import React from "react";
-import image from "../assets/download.jpg";
-import { Link } from "react-router-dom";
+import React, { useContext, useEffect } from "react";
+import { Context } from "../context/contextApi";
+import VideoCard from "./VideoCard"
 
 const Feed = () => {
+  const { loading, searchResults } = useContext(Context);
+
+  useEffect(() => {
+    document.getElementById("root").classList.remove("custom-h");
+  }, []);
+
   return (
     <div className="flex h-full w-full bg-slate-500 items-center justify-center">
       <div className="grid mt-10 mb-10 p-10 gap-3 w-[1150px] rounded-xl items-center bg-slate-400">
         <div className="flex flex-wrap justify-center gap-4 items-center cursor-pointer">
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
-          <Link to="/video">
-            <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
-              <img
-                src={image}
-                alt="vedoio-image"
-                className="h-[300px] w-[200px] rounded-md"
-              />
-              <h2 className="absolute bottom-3  left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-2 text-white font-semibold text-xl text-center">
-                title
-              </h2>
-            </div>
-          </Link>
+          {!loading &&
+            searchResults &&
+            searchResults.map((item) => {
+              if (item?.type !== "video") {
+                return false;
+              }
+              return <VideoCard key={item?.video?.videoId} video={item?.video} />;
+            })}
         </div>
       </div>
     </div>
