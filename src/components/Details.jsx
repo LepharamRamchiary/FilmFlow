d13b3b702e6bf3c5598a6124f6a3cfd7d4429467
Lepha <lepharamchiary@gmail.com>
2024-05-16 21:05:10 +0530

    .

--- src/components/Details.jsx
+++ src/components/Details.jsx
@@ -1,22 +1,48 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import ReactPlayer from "react-player";
+
+import { fetchDataFromApi } from "../utils/api";
+import { Context } from "../context/contextApi";
 import image from "../assets/download.jpg";
 
 const Details = () => {
+  const [video, setVideo] = useState();
+  const { id } = useParams();
+  const { setLoading } = useContext(Context);
+
+  useEffect(() => {
+    document.getElementById("root").classList.add("custom-h");
+    fetchVideoDetails();
+  }, [id]);
+
+  const fetchVideoDetails = () => {
+    setLoading(true);
+    fetchDataFromApi(`video/details/?id=${id}`).then((res) => {
+      console.log(res);
+      setVideo(res);
+      setLoading(false);
+    });
+  };
+
   return (
     <div className="flex bg-slate-800 md:h-full md:w-full">
       <div className="flex flex-col sm:mt-3 sm:mb-3 sm:ml-4 sm:p-5 mt-3 ml-3 mr-3 p-3 gap-4 md:mt-12 md:mb-12 md:ml-10 md:mr-10 md:p-10 md:flex-row sm:flex-col">
         <div className="w-full md:w-1/2">
-          <h1 className="text-white font-semibold text-2xl">Title</h1>
-          <span className="text-white">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis
-            ducimus fugit tenetur reprehenderit in impedit temporibus. Laborum
-            cupiditate doloremque qui excepturi earum expedita impedit aut,
-            assumenda ipsa nesciunt voluptatibus quidem.
-          </span>
+          <h1 className="text-white font-semibold text-2xl line-clamp-1">
+            {video?.title}
+          </h1>
+          <p className="text-white mt-3">{video?.description}</p>
           <h3 className="text-green-400 font-semibold mt-5">IMDB rating</h3>
         </div>
         <div className="flex sm:w-1/2 md:w-1/2 lg:w-1/2 items-center justify-center md:h-[400px] lg:h-[500px]">
-          <img src={image} className="h-full w-full md:h-[300px] lg:h-[400px] md:mb-12" alt="image" />
+          <ReactPlayer
+            url={`https://www.youtube.com/watch?v=${id}`}
+            controls
+            style={{ backgroundColor: "#000000" }}
+            className="h-[200px] w-full md:h-[400px] lg:h-[400px]"
+            // playing={true}
+          />
         </div>
       </div>
     </div>
