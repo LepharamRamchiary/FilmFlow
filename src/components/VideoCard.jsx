b0fced8ee474bfc3daa2e1a9917348e9aaae843d
Lepha <lepharamchiary@gmail.com>
2024-05-16 21:05:41 +0530

    .

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const VideoCard = ({ video }) => {
   return (
     <Link to={`/video/${video?.videoId}`}>
-      <div className="relative h-[300px] w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
+      <div className="relative h-[250px] md:h-[300px] md:w-[200px] lg:h-[300px] lg:w-[200px] rounded-md shadow-[rgba(13,_38,_76,_0.19)_0px_9px_20px]">
           <img
             src={video?.thumbnails?.[0].url}
             alt={video?.title}
-            className="h-[300px] w-[200px] rounded-md"
+            className="h-[250px] md:h-[300px] md:w-[200px] lg:h-[300px] lg:w-[200px] rounded-md"
           />
-        <h2 className="absolute bottom-3 left-1/2 transform -translate-x-1/2 -translate-y-1/2 line-clamp-1 text-white font-semibold text-sm text-start">
+        <h2 className="absolute bottom-3 p-2 line-clamp-0 text-white text-sm text-center justify-centerst">
           {video?.title}
         </h2>
       </div>
