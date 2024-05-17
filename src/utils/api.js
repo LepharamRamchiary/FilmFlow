e3a71e4b87a1a880de41d7a2f409ec76753da77a
Lepha <lepharamchiary@gmail.com>
2024-05-17 13:12:26 +0530

    .

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,17 +1,26 @@
-import axios from 'axios';
-const BASE_URL = 'https://api.apilayer.com/youtube';
+import axios from "axios";
+
+const BASE_URL = "https://streaming-availability.p.rapidapi.com/shows/search/filters";
 
 const options = {
   params: {
-    q: '{q}' 
+    country: '<REQUIRED>',
+    show_type: 'movie',
+    series_granularity: 'show',
+    order_by: 'original_title',
+    output_language: 'en',
+    order_direction: 'asc',
+    genres_relation: 'and'
   },
   headers: {
-    'apikey': process.env.REACT_APP_MOVIE_API_KEY,
-    'Content-Type': 'application/json'
+    'X-RapidAPI-Key': process.env.REACT_APP_MOVIE_API_KEY,
+    'X-RapidAPI-Host': 'streaming-availability.p.rapidapi.com'
   }
 };
 
 export const fetchDataFromApi = async (url) => {
-  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
-  return data;
-}
+    const {response} = await axios.get(`${BASE_URL}/${url}`, options);
+    return response;
+};
+
+
