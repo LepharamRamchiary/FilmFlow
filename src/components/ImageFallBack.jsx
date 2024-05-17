0cb354b5f52fca94fef5dfe907edf1864a98527c
Lepha <lepharamchiary@gmail.com>
2024-05-17 22:46:02 +0530

    .

--- src/components/ImageFallBack.jsx
+++ src/components/ImageFallBack.jsx
@@ -5,7 +5,7 @@ const ImageFallBack = ({ src, alt, fallbackSrc, ...props }) => {
     e.target.src = fallbackSrc;
   };
 
-  return <img src={src} alt={alt} onError={handleError} {...props} />;
+  return <img src={src} alt={alt} onError={handleError} {...props} className="h-[250px] md:h-[300px] md:w-[200px] lg:h-[300px] lg:w-[200px] rounded-md justify-center items-center"/>;
 };
 
 export default ImageFallBack;
