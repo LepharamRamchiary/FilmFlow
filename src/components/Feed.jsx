f9a05aff82be47643bf64e0d1e8a2b3ff7cd8217
Lepha <lepharamchiary@gmail.com>
2024-05-17 15:45:09 +0530

    .

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import { Context } from "../context/contextApi";
-import VideoCard from "./VideoCard"
+import VideoCard from "./VideoCard";
 
 const Feed = () => {
   const { loading, searchResults } = useContext(Context);
@@ -8,7 +8,10 @@ const Feed = () => {
   useEffect(() => {
     document.getElementById("root").classList.remove("custom-h");
   }, []);
-
+  useEffect(() => {
+    console.log("Loading:", loading);
+    console.log("Search Results:", searchResults);
+  }, [loading, searchResults]);
   return (
     <div className="flex h-full w-full bg-slate-500 items-center justify-center">
       <div className="grid mt-10 mb-10 p-10 gap-3 w-[1150px] rounded-xl items-center bg-slate-400">
@@ -16,10 +19,12 @@ const Feed = () => {
           {!loading &&
             searchResults &&
             searchResults.map((item) => {
-              if (item?.type !== "video") {
+              if (item?.type !== "movie") {
                 return false;
               }
-              return <VideoCard key={item?.video?.videoId} video={item?.video} />;
+              return (
+                <VideoCard key={item?.imdbid} video={item} />
+              );
             })}
         </div>
       </div>
